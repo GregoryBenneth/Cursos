e9ef329e387e040d3bac97cc945d78c51aad3172
gregoryd <uchihamadaraxyz@gmail.com>
2022-08-24 17:53:21 -0300

    lessons new

---
+++ Funções/function_param.js
@@ -0,0 +1,13 @@
+function somar(a = 0, b = 0, c = 0, d = 0) {
+    return a + b + c + d;
+
+};
+
+console.log("O resultado é:", somar(1, 2, 3, 45));
+console.log("O resultado é:", somar(1, 2, 3));
+console.log("O resultado é:", somar(1, 2));
+console.log("O resultado é:", somar(1, ));
+console.log("O resultado é:", somar());
+
+
+console.log("Fim da operação!");
